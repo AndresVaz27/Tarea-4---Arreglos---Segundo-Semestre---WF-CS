7534fc5ee2e49fcb22a230552df2c4c9ed2d77e9
Andres <andresvazquezo1@hotmail.com>
2023-02-19 11:57:29 -0600

    Add project files.

---
+++ Tarea 4 - Arreglos - Segundo Semestre - WF CS/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Tarea_4___Arreglos___Segundo_Semestre___WF_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lbContactos = new System.Windows.Forms.ListBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblArregloLenght = new System.Windows.Forms.Label();
+            this.txtArregloLenght = new System.Windows.Forms.TextBox();
+            this.btnEstablecer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(534, 204);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(137, 38);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(215, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(201, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(215, 181);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(201, 22);
+            this.txtTelefono.TabIndex = 3;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(215, 229);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(201, 22);
+            this.txtCorreo.TabIndex = 4;
+            // 
+            // lbContactos
+            // 
+            this.lbContactos.FormattingEnabled = true;
+            this.lbContactos.ItemHeight = 16;
+            this.lbContactos.Location = new System.Drawing.Point(53, 324);
+            this.lbContactos.Name = "lbContactos";
+            this.lbContactos.Size = new System.Drawing.Size(602, 228);
+            this.lbContactos.TabIndex = 5;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(59, 85);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(115, 24);
+            this.lblNombre.TabIndex = 6;
+            this.lblNombre.Text = "Nombre:";
+            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(59, 177);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(126, 26);
+            this.lblTelefono.TabIndex = 7;
+            this.lblTelefono.Text = "Telefono:";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreo.Location = new System.Drawing.Point(60, 225);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(85, 25);
+            this.lblCorreo.TabIndex = 8;
+            this.lblCorreo.Text = "Correo:";
+            // 
+            // lblArregloLenght
+            // 
+            this.lblArregloLenght.AutoSize = true;
+            this.lblArregloLenght.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArregloLenght.Location = new System.Drawing.Point(61, 32);
+            this.lblArregloLenght.Name = "lblArregloLenght";
+            this.lblArregloLenght.Size = new System.Drawing.Size(247, 16);
+            this.lblArregloLenght.TabIndex = 9;
+            this.lblArregloLenght.Text = "Número de Contactos por Agregar:";
+            // 
+            // txtArregloLenght
+            // 
+            this.txtArregloLenght.Location = new System.Drawing.Point(357, 32);
+            this.txtArregloLenght.Name = "txtArregloLenght";
+            this.txtArregloLenght.Size = new System.Drawing.Size(132, 22);
+            this.txtArregloLenght.TabIndex = 10;
+            // 
+            // btnEstablecer
+            // 
+            this.btnEstablecer.Location = new System.Drawing.Point(540, 24);
+            this.btnEstablecer.Name = "btnEstablecer";
+            this.btnEstablecer.Size = new System.Drawing.Size(137, 38);
+            this.btnEstablecer.TabIndex = 11;
+            this.btnEstablecer.Text = "Establecer";
+            this.btnEstablecer.UseVisualStyleBackColor = true;
+            this.btnEstablecer.Click += new System.EventHandler(this.btnEstablecer_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Fecha de Nacimiento (\"dd/mm/aaaa\"):";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(476, 138);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(201, 22);
+            this.txtFecha.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 606);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEstablecer);
+            this.Controls.Add(this.txtArregloLenght);
+            this.Controls.Add(this.lblArregloLenght);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lbContactos);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Form1";
+            this.Text = "WF CS - Arreglos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.ListBox lbContactos;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Label lblArregloLenght;
+        private System.Windows.Forms.TextBox txtArregloLenght;
+        private System.Windows.Forms.Button btnEstablecer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFecha;
+    }
+}
+
---
+++ Tarea 4 - Arreglos - Segundo Semestre - WF CS/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tarea_2___Segundo_Semestre;
+
+namespace Tarea_4___Arreglos___Segundo_Semestre___WF_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {}
+
+
+
+        int cantidadContactos;
+        int siguienteIndex = 0;
+        Contacto[] contactos;
+
+        // ESTABLECER TAMAÑO DEL ARREGLO.
+        private void btnEstablecer_Click(object sender, EventArgs e)
+        {
+            cantidadContactos = int.Parse(txtArregloLenght.Text);
+            contactos = new Contacto[cantidadContactos];
+        }
+
+        // AGREGAR UN NUEVO CONTACTO.
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (contactos == null) { MessageBox.Show("Establezca el número de contactos por agregar.");}
+            else if (siguienteIndex >= contactos.Length)
+            {
+                MessageBox.Show("La cantidad de contactos por agregar se completó.");
+               
+            }
+            else
+            {
+                Contacto nuevo = new Contacto();
+                nuevo.Nombre = txtNombre.Text;
+                nuevo.Telefono = txtTelefono.Text;
+                DateTime fechaNacimiento;
+                if (DateTime.TryParseExact(txtFecha.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out fechaNacimiento))
+                {
+                    nuevo.FechaNacimiento = fechaNacimiento;
+                }
+                else
+                {
+                    MessageBox.Show("La fecha de nacimiento debe tener el formato dd/MM/yyyy");
+                    return;
+                }
+                nuevo.Correo= txtCorreo.Text;
+
+                contactos[siguienteIndex] = nuevo;
+                lbContactos.Items.Add(nuevo.ToString());
+                siguienteIndex++;
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {}
+    }
+}
